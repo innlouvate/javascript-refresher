c95f6a3f642ed8baf5278755e26597ae8ae07757
innlouvate <l.j.franklin85@googlemail.com>
2016-03-14 19:40:08 +0000

    round 1

--- questions.js
+++ questions.js
@@ -1,93 +1,130 @@
-var selectElementsStartingWithA = function(array) {
-  return 'Write your method here';
-}
+// var firstLetter = function(word, letter) {
+//   return word.startsWith(letter);
+// }
 
 var selectElementsStartingWithA = function(array) {
-  return 'Write your method here';
+  return array.filter(function(item, index) {
+    return item.charAt(0) === 'a';
+  });
 }
 
 var selectElementsStartingWithVowel = function(array) {
-  return 'Write your method here';
+  return array.filter(function(item, index) {
+    return (/^[aeiou]$/i).test(item.charAt(0));
+  });
 }
 
 var removeNullElements = function(array) {
-  return 'Write your method here';
+  return array.filter(function(n){ return n != undefined });
 }
 
 var removeNullAndFalseElements = function(array) {
-  return 'Write your method here';
+  return array.filter(function(n){ return n != undefined && n !== false });
 }
 
 var reverseWordsInArray = function(array) {
-  return 'Write your method here';
+  return array.map(function(x) { return x.split('').reverse().join('')})
 }
 
 var everyPossiblePair = function(array) {
-  return 'Write your method here';
+
 }
 
 var allElementsExceptFirstThree = function(array) {
-  return 'Write your method here';
+  return array.filter(function(item, index) {
+    if(index > 2) {
+      return item;
+    }
+  });
 }
 
 var addElementToBeginning = function(array, element) {
-  return 'Write your method here';
+  array.splice(0,0, element);
+  return array;
 }
 
 var sortByLastLetter = function(array) {
-  return 'Write your method here';
+  return array.sort(function(a, b){
+    if(a.charAt(a.length-1) < b.charAt(b.length-1)) return -1;
+    if(a.charAt(a.length-1) > b.charAt(b.length-1)) return 1;
+    return 0;
+  });
 }
 
 var getFirstHalf = function(string) {
-  return 'Write your method here';
+  return string.slice(0, Math.ceil(string.length/2));
+}
+var getLastHalf = function(string) {
+  return string.slice(Math.floor(string.length/2), string.length);
 }
 
 var makeNegative = function(number) {
-  return 'Write your method here';
+  return Math.abs(number) *(-1);
 }
 
 var numberOfPalindromes = function(array) {
-  return 'Write your method here';
+  return array.filter(function(word) {
+    return getFirstHalf(word)===((getLastHalf(word)).split('').reverse().join(''))
+  }).length
 }
 
 var shortestWord = function(array) {
-  return 'Write your method here';
+  return array.sort(function(a,b) { return a.length > b.length } )[0];
 }
 
 var longestWord = function(array) {
-  return 'Write your method here';
+  return array.sort(function(a,b) { return a.length < b.length } )[0];
 }
 
 var sumNumbers = function(array) {
-  return 'Write your method here';
+  return array.reduce(function(total, num) { return total += num });
 }
 
 var repeatElements = function(array) {
-  return 'Write your method here';
+  return array.concat(array);
+}
+
+var removeDuplicateElements = function(array) {
+  return array.filter(function(item, index) { return array.indexOf(item) == index });
 }
 
 var stringToNumber = function(string) {
-  return 'Write your method here';
+  return parseInt(string);
 }
 
 var calculateAverage = function(array) {
-  return 'Write your method here';
+  return sumNumbers(array) / array.length;
 }
 
 var getElementsUntilGreaterThanFive = function(array) {
-  return 'Write your method here';
+  return array.slice(0,6);
 }
 
 var convertArrayToObject = function(array) {
-  return 'Write your method here';
+  var keyselect = array.filter(function(item, index) { return index%2===0})
+  return keyselect.reduce(function(result, item) {
+    result[item] = array[array.indexOf(item)+1];
+    return result;
+  }, {});
+}
+
+var removeDuplicateCharacters = function(string) {
+  return string.split('').filter(function(item, index, array) {
+    return array.indexOf(item) == index
+  }).join('');
 }
 
 var getAllLetters = function(array) {
-  return 'Write your method here';
+  return removeDuplicateCharacters(array.join('')).split('').sort();
 }
 
 var swapKeysAndValues = function(object) {
-  return 'Write your method here';
+  return Object.keys(object)                //get the keys as an array
+    // .filter(o.hasOwnProperty.bind(example)) //safety check (optional)
+    .reduce(function(newObj, key) {                  //build up new object
+        newObj[object[key]] = key;
+        return newObj;
+    }, {});
 }
 
 var sumKeysAndValues = function(object) {
